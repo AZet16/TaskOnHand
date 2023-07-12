02fc5310b145b4b07de0a49182ac179924bf6022
AisanaZh177 <101815204+AisanaZh177@users.noreply.github.com>
2023-07-12 19:13:59 +0100

    added new task function and redirection back

--- src/main/java/com/example/springboot311tasksonhandapplicaton/controllers/TaskFormController.java
+++ src/main/java/com/example/springboot311tasksonhandapplicaton/controllers/TaskFormController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.springboot311tasksonhandapplicaton.models.TaskItem;
 import com.example.springboot311tasksonhandapplicaton.repositories.TaskItemRepo;
 import com.example.springboot311tasksonhandapplicaton.services.TaskItemService;
 
@@ -13,7 +14,21 @@ public class TaskFormController {
     private TaskItemService taskItemService;
 
     @GetMapping("/add-task")
-    public String createForm(){
+    public String createForm(TaskItem taskItem) {
         return "generating-new-task";
+    }
+    
+    @GetMapping("/new-task")
+    public String createNewTask(@Valid TaskItem taskItem, BindingResult result, Model model) {
+        TaskItem task1 = new TaskItem();
+        task1.setDescription(taskItem.getDescription());
+        //task1.setCategory(taskItem.getCategory());
+        //task1.setCurrentState(taskItem.getCurrentState());
+        //task1.setIsDone(taskItem.getIsDone());
+
+        taskItemService.save(task1);
+        
+        return "redirect:/";
+
     }
 }
